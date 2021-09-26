61262dc81a30fa348c7c78fbb92a1f29846fc720
gyuri <fnffkdll@naver.com>
2021-09-26 23:32:03 +0900

    header 추가

---
+++ src/component/Header.js
@@ -0,0 +1,15 @@
+import {Link} from "react-router-dom";
+
+export default function Header(){
+    return (
+        <header className="header">
+            <h1 className="logo">
+                <Link to="/">🍊Study</Link>
+            </h1>
+            <div className="util">
+                <Link to="" className="utilLink">➕ Skill</Link>
+                <Link to="" className="utilLink">➕ Study</Link>
+            </div>
+        </header>
+    );
+}
