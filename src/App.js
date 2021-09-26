e11f3f1d486136c912ea81a7977a4e2c461a69bd
gyuri <fnffkdll@naver.com>
2021-09-27 02:08:01 +0900

    skill page 생성 (dummy, fillter, useParams 적용)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import "./contents.css"
 import Header from "./component/Header";
 import Main from "./component/Main";
+import Skill from "./component/Skill";
 
 function App() {
     return ( 
@@ -11,6 +12,9 @@ function App() {
                 <Switch>
                     <Route exact path="/">
                         <Main />
+                    </Route>
+                    <Route path="/skill/:skill">
+                        <Skill />
                     </Route>
                 </Switch>
             </div>
