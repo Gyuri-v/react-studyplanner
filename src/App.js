16a8acaa8dadd7a35dc79b0b3458c79158cdb0d3
gyuri <fnffkdll@naver.com>
2021-09-27 01:04:59 +0900

    main - ui추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import {BrowserRouter, Router, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import "./contents.css"
 import Header from "./component/Header";
+import Main from "./component/Main";
 
 function App() {
     return ( 
         <BrowserRouter>
             <div className = "App" >
                 <Header></Header>
+                <Switch>
+                    <Route exact path="/">
+                        <Main />
+                    </Route>
+                </Switch>
             </div>
         </BrowserRouter>
     );
