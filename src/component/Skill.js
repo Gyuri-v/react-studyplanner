e4d529ae9995c5414bd216b239df153f457120fa
gyuri <fnffkdll@naver.com>
2021-09-27 23:52:18 +0900

    skill page - useEffect, fetch 적용

--- src/component/Skill.js
+++ src/component/Skill.js
@@ -1,18 +1,31 @@
 import { Link } from "react-router-dom";
 import { useParams } from "react-router";
-import dummy from "../db/data.json"
+//import dummy from "../db/data.json"
+import { useEffect, useState } from "react";
 
 export default function Skill(){
 
-    const skillName = useParams().skill;
-    const skillList = dummy.studies.filter(skill => skill.skill === skillName);
+    const skill = useParams().skill;
+    const [skills, setSkills] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:3001/studies?skill=${skill}`)
+        .then(res => {
+            return res.json()
+        })
+        .then(data => {
+            setSkills(data);
+        })
+    })
+
+    console.log(skills);
 
     return (
         <main className="container skill">
             <section>
-                <h3 className="cmTitle">📚 {skillName} Study </h3>
+                <h3 className="cmTitle">📚 {skill} Study </h3>
                 <ul className="studyList">
-                {skillList.map(study => (
+                {skills.map(study => (
                         <li key={study.id}>
                             <Link to={`/study/${study.study}`} style={{borderColor: study.bdColor }}> 
                                 <span className="title">{study.study}</span> 
