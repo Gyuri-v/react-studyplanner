e34261a7555dab1475e6762426eb7c677ff89eba
GYURI <fnffkdll@naver.com>
2022-03-04 16:55:49 +0900

    pages 사용을 위한 세팅

--- src/component/Skill.js
+++ src/component/Skill.js
@@ -1,38 +1,39 @@
-import { useHistory, useParams } from "react-router";
-import StudyList from "../component/StudyList";
-import React from "react";
-import dummy from "../db/data.json";
+import { useHistory, useParams } from 'react-router';
+import StudyList from '../component/StudyList';
+import React from 'react';
+import dummy from '../db/data.json';
 
-export default function Skill(){
+export default function Skill() {
+  const skill = useParams().skill;
+  const skillName = skill;
+  const skillId = dummy.skills.filter((skill) => skill.skill === skillName)[0]
+    .id;
+  const history = useHistory();
 
-    const skill = useParams().skill;
-    const skillName = skill;
-    const skillId = (dummy.skills.filter( skill => skill.skill === skillName ))[0].id;
-    const history = useHistory();
-
-    function Del(){
-        if( window.confirm('정말로 삭제하시겠습니까?') ){
-            fetch(`http://localhost:3001/skills/${skillId}`,{
-                method: 'DELETE',
-            })
-            .then(res => {
-                if( res.ok ){
-                    alert('삭제가 완료 되었습니다.');
-                    history.push(`/`);
-                }
-            })
+  function Del() {
+    if (window.confirm('정말로 삭제하시겠습니까?')) {
+      fetch(`http://localhost:3001/skills/${skillId}`, {
+        method: 'DELETE',
+      }).then((res) => {
+        if (res.ok) {
+          alert('삭제가 완료 되었습니다.');
+          history.push(`/`);
         }
+      });
     }
+  }
 
-    return (
-        <main className="container skill">
-            <section>
-                <h3 className="cmTitle">
-                    📚 {skill} Study
-                    <button className="btnDel" onClick={Del}>삭제</button>
-                </h3>
-                <StudyList skill={skill} />
-            </section>
-        </main>
-    );
+  return (
+    <main className="container skill">
+      <section>
+        <h3 className="cmTitle">
+          📚 {skill} Study
+          <button className="btnDel" onClick={Del}>
+            삭제
+          </button>
+        </h3>
+        <StudyList skill={skill} />
+      </section>
+    </main>
+  );
 }
